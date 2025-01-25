77e838d15b37684d7f7d9f822df2684dba440538
Barakat Gadban <barakatg88@gmail.com>
2025-01-25 10:31:34 +0200

    added spa files

---
+++ src/etc/spa/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import {ServersListComponent} from './components/servers-list/servers-list.component';
+
+export const routes: Routes = [
+  {
+    path: 'servers',
+    component: ServersListComponent
+  }
+];
---
+++ src/etc/spa/src/app/components/servers-list/servers-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers-list',
+  imports: [],
+  templateUrl: './servers-list.component.html',
+  styleUrl: './servers-list.component.sass'
+})
+export class ServersListComponent {
+
+}
